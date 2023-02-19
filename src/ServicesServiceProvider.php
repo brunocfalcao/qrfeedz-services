adec02bf9204ed6cfeee7b69ec374157ee9e8269
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-02-19 12:19:05 +0100

    Update ServicesServiceProvider.php

--- src/ServicesServiceProvider.php
+++ src/ServicesServiceProvider.php
@@ -3,12 +3,17 @@
 namespace QRFeedz\Services;
 
 use Illuminate\Support\ServiceProvider;
+use QRFeedz\Services\Commands\PintCommand;
 
 class ServicesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        /*
+        $this->commands([
+            PintCommand::class
+        ]);
+        */
     }
 
     public function register()
