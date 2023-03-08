f13f7f612e19730a456efb69c7568970d9e246c5
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-08 16:27:17 +0100

    wip

--- src/Workflows/ViaPlaceCreation.php
+++ src/Workflows/ViaPlaceCreation.php
@@ -2,8 +2,8 @@
 
 /**
  * Creates a full data structure starting by the place, in the passed type.
- * In detail, we pass the data for the place, and it will create an
- * organization, a place, a default questionnaire and 3 locales.
+ * In detail, we pass the data for the place, and it will create a
+ * client, a place, a default questionnaire and 3 locales.
  *
  * Can create a Restaurant, a Store, or an Hotel (building).
  */
