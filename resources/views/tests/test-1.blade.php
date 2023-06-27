4f5414f13195c234a0b6dc899a7ba2ec41573144
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-06-27 20:46:40 +0200

    Create test-1.blade.php

---
+++ resources/views/tests/test-1.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        @media only screen and (max-width: 600px) {
+            /* CSS for mobile devices */
+            .container {
+                width: 100% !important;
+                padding: 20px !important;
+            }
+        }
+        body {
+            margin: 60px 0 0;
+            padding: 0;
+            background-color: #f2f2f2;
+            font-family: Calibri, 'Open Sans', sans-serif;
+            line-height: 1.5;
+        }
+        .button-container {
+            text-align: center;
+        }
+        .button {
+            display: inline-block;
+            margin-top: 20px;
+            padding: 12px 24px;
+            background-color: #2196F3;
+            color: #ffffff;
+            text-decoration: none;
+            border-radius: 4px;
+        }
+    </style>
+</head>
+<body>
+    <pre style="font-size: 1px; color: #ffffff; display: none !important;">[Preview Subject]</pre>
+    <table cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px; margin: 0 auto;">
+        <tr>
+            <td align="center" style="padding: 20px 0; background-color: #ffffff;">
+                <img src="https://placehold.co/100x60" alt="QRFeedz Logo" style="display: block; width: 100px; height: 60px;">
+                <h1 style="margin-top: 20px;">QRFeedz</h1>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#ffffff" style="padding: 20px;">
+                <p><strong style="font-size: 18px;">Welcome, User!</strong></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium eros non nisi commodo viverra. Curabitur convallis, dui non vestibulum vulputate, lorem quam tincidunt odio, id convallis enim ligula nec metus.</p>
+                <p>Sed faucibus nunc diam, sed semper sem facilisis ut. Suspendisse convallis felis a lacus ultrices, at dictum felis semper. Sed pulvinar eros id lacus efficitur, sit amet bibendum purus blandit.</p>
+                <div class="button-container">
+                    <a href="https://www.publico.pt" class="button" target="_blank">Click Here</a>
+                </div>
+                <p>Best regards,<br>QRFeedz Team</p>
+            </td>
+        </tr>
+        <tr>
+            <td align="center" style="padding: 10px 0; background-color: #f7f7f7;">
+                <pre style="margin: 0; padding: 10px;">All rights reserved</pre>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
