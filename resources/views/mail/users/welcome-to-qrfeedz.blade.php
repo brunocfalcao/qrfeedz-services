bf3b6dc4b7dd55579fa1f442848ab9a1442fe0dc
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-06-28 10:10:43 +0200

    wip

--- resources/views/mail/users/welcome-to-qrfeedz.blade.php
+++ resources/views/mail/users/welcome-to-qrfeedz.blade.php
@@ -1,11 +1,27 @@
 <x-mail::message>
-# Introduction
+# Hello
 
-The body of your message.
+This is a sample markdown email.
 
-<x-mail::button :url="''">
-Button Text
+<x-mail::button :url="$url">
+{{ $buttonText }}
 </x-mail::button>
+
+<x-mail::panel>
+This is a panel component. It's used to highlight some important content.
+</x-mail::panel>
+
+<x-mail::table>
+| Laravel       | Table         | Example  |
+| ------------- |:-------------:| --------:|
+@foreach ($tableData as $data)
+| {{ $data[0] }} | {{ $data[1] }} |
+@endforeach
+</x-mail::table>
+
+<x-mail::subcopy>
+This is some subcopy text. It's typically used for providing additional information or clarification.
+</x-mail::subcopy>
 
 Thanks,<br>
 {{ config('app.name') }}
--- src/Mail/Users/WelcomeToQRFeedz.php
+++ src/Mail/Users/WelcomeToQRFeedz.php
@@ -10,6 +10,15 @@ class WelcomeToQRFeedz extends QRFeedzMail
     {
         $this->subject = 'This is a test mail';
         $this->markdown = 'qrfeedz-services::mail.users.welcome-to-qrfeedz';
+        $this->data = [
+                        'url' => 'http://example.com',
+                        'buttonText' => 'Visit our website',
+                        'tableData' => [
+                            ['First row', 'Data'],
+                            ['Second row', 'More data'],
+                            ['Third row', 'Even more data'],
+                        ],
+                    ];
     }
 
     public function attachments(): array
