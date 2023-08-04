e0ca054599be27ff21f98bd7b8a312e333220218
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-04 09:40:29 +0200

    wip

--- resources/overrides/config/qrfeedz.php
+++ resources/overrides/config/qrfeedz.php
@@ -9,5 +9,18 @@ return [
          * If it's on, then it will send all possible notifications.
          */
         'allow_notifications' => env('QRFEEDZ_ALLOW_NOTIFICATIONS', false),
+
+        'mails' => [
+
+            'no_reply' => [
+                'name' => env('QRFEEDZ_NO_REPLY_EMAIL_NAME'),
+                'email' => env('QRFEEDZ_NO_REPLY_EMAIL_MAIL'),
+            ],
+
+            'contact' => [
+                'name' => env('QRFEEDZ_CONTACT_EMAIL_NAME'),
+                'email' => env('QRFEEDZ_CONTACT_EMAIL_MAIL'),
+            ],
+        ],
     ],
 ];
