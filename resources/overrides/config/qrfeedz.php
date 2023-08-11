fb5f9544d1550824d3d0c5a1578b5c856c08a362
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-11 07:32:02 +0200

    wip

--- resources/overrides/config/qrfeedz.php
+++ resources/overrides/config/qrfeedz.php
@@ -7,6 +7,21 @@ return [
         'url' => env('QRFEEDZ_URL', 'https://{xxx}.qrfeedz.com'),
 
         /**
+         * No matter in what scenario we are (backend, frontend, etc) we will
+         * always load the routes if the value is true. Mostly for testing
+         * purposes, in production all keys should be false so the framework
+         * will automatically load the right routes given the domain name.
+         *
+         * REMINDER: You shouldn't have backend and frontend routes loaded
+         *           simultaneously.
+         */
+        'always_route' => [
+            'backend' => true,
+            'frontend' => false,
+            'testing' => true,
+        ],
+
+        /**
          * Should QRFeedz send notifications, globally speaking.
          * If it's on, then it will send all possible notifications.
          */
