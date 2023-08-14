e199ec839b9fbc3b0e23b2494efc1d0b5d324a7f
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-08-14 02:37:34 +0200

    wip

---
+++ resources/views/mail/templates/metronic-adjusted-2.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Metronic - The World's #1 Selling Bootstrap Admin Template by Keenthemes</title>
+    <meta name="description" content="The most advanced Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. Grab your copy now and get life-time updates for free." />
+    <meta name="keywords" content="metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <!--begin::Fonts(mandatory for all pages)-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+    <!--end::Fonts-->
+    <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+    <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <!--end::Global Stylesheets Bundle-->
+</head>
+<body id="kt_body" class="app-blank">
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root" id="kt_app_root">
+        <!--begin::Wrapper-->
+        <div class="d-flex flex-column flex-column-fluid">
+            <!--begin::Body-->
+            <div class="scroll-y flex-column-fluid px-10 py-10" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_header_nav" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true" style="background-color:#D5D9E2; --kt-scrollbar-color: #d9d0cc; --kt-scrollbar-hover-color: #d9d0cc">
+                <!--begin::Email template-->
+                <style>html,body { padding:0; margin:0; font-family: Inter, Helvetica, "sans-serif"; } a:hover { color: #009ef7; }</style>
+                <div id="kt_app_body_content" style="background-color:#D5D9E2; font-family:Arial,Helvetica,sans-serif; line-height: 1.5; min-height: 100%; font-weight: normal; font-size: 15px; color: #2F3044; margin:0; padding:0; width:100%;">
+                    <!--begin::Email content-->
+                    <div style="background-color:#ffffff; padding: 45px 0 34px 0; border-radius: 24px; margin:40px auto; max-width: 600px;">
+                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" height="auto" style="border-collapse:collapse">
+                            <tbody>
+                                <tr>
+                                    <td align="center" valign="center" style="text-align:center; padding-bottom: 10px">
+                                        <!--begin:Email content-->
+                                        <div style="text-align:center; margin:0 15px 34px 15px">
+                                            <!--begin:Logo-->
+                                            <div style="margin-bottom: 10px">
+                                                <a href="https://keenthemes.com" rel="noopener" target="_blank">
+                                                    <img alt="Logo" src="assets/media/email/logo-1.svg" style="height: 35px" />
+                                                </a>
+                                            </div>
+                                            <!--end:Logo-->
+                                            <!--begin:Media-->
+                                            <div style="margin-bottom: 15px">
+                                                <img alt="Logo" src="assets/media/email/icon-positive-vote-1.svg" />
+                                            </div>
+                                            <!--end:Media-->
+                                            <!--begin:Text-->
+                                            <div style="font-size: 14px; font-weight: 500; margin-bottom: 27px; font-family:Arial,Helvetica,sans-serif;">
+                                                <p style="margin-bottom:9px; color:#181C32; font-size: 22px; font-weight:700">Hey Marcus, thanks for signing up!</p>
+                                                <p style="margin-bottom:2px; color:#7E8299">Lots of people make mistakes while creating</p>
+                                                <p style="margin-bottom:2px; color:#7E8299">paragraphs. Some writers just put whitespace in</p>
+                                                <p style="margin-bottom:2px; color:#7E8299">their text in random places</p>
+                                            </div>
+                                            <!--end:Text-->
+                                            <!--begin:Action-->
+                                            <a href='../../demo1/dist/authentication/general/welcome.html' target="_blank" style="background-color:#50cd89; border-radius:6px;display:inline-block; padding:11px 19px; color: #FFFFFF; font-size: 14px; font-weight:500;">Activate Account</a>
+                                            <!--begin:Action-->
+                                        </div>
+                                        <!--end:Email content-->
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" valign="center" style="font-size: 13px; padding:0 15px; text-align:center; font-weight: 500; color: #A1A5B7;font-family:Arial,Helvetica,sans-serif">
+                                        <p>&copy; Copyright KeenThemes.
+                                        <a href="https://keenthemes.com" rel="noopener" target="_blank" style="font-weight: 600;font-family:Arial,Helvetica,sans-serif">Unsubscribe</a>&nbsp; from newsletter.</p>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!--end::Email content-->
+                </div>
+                <!--end::Email template-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Wrapper-->
+    </div>
+    <!--end::Root-->
+</body>
+</html>
---
+++ resources/views/mail/templates/metronic-adjusted-3.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Metronic</title>
+    <meta name="description" content="Metronic - The World's #1 Selling Bootstrap Admin Template">
+    <meta name="keywords" content="metronic, bootstrap, admin template, web design">
+    <link rel="canonical" href="https://preview.keenthemes.com/metronic8">
+    <link rel="shortcut icon" href="assets/media/logos/favicon.ico">
+    <style>html,body{padding:0;margin:0}a:hover{color:#009ef7}</style>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700">
+    <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css">
+</head>
+<body id="kt_body" style="margin:0;padding:0;font-family:Inter,Helvetica,sans-serif" class="app-blank">
+    <div class="d-flex flex-column flex-root" id="kt_app_root">
+        <div class="d-flex flex-column flex-column-fluid">
+            <div class="bg-body d-flex flex-column-auto justify-content-center align-items-start py-4 px-10 overflow-auto" id="kt_app_header_nav">
+            </div>
+            <div class="scroll-y flex-column-fluid px-10 py-10" style="background-color:#D5D9E2;--kt-scrollbar-color:#d9d0cc;--kt-scrollbar-hover-color:#d9d0cc">
+                <div id="kt_app_body_content" style="background-color:#D5D9E2;font-family:Arial,Helvetica,sans-serif;line-height:1.5;min-height:100%;font-weight:normal;font-size:15px;color:#2F3044;width:100%">
+                    <div style="background-color:#ffffff;padding:45px 0 34px 0;border-radius:24px;margin:40px auto;max-width:600px">
+                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" height="auto" style="border-collapse:collapse">
+                            <tbody>
+                                <tr>
+                                    <td align="center" valign="center" style="text-align:center;padding-bottom:10px">
+                                        <div style="text-align:center;margin:0 15px 34px 15px">
+                                            <div style="margin-bottom:10px">
+                                                <a href="https://keenthemes.com" rel="noopener" target="_blank">
+                                                    <img alt="Logo" src="assets/media/email/logo-1.svg" style="height:35px">
+                                                </a>
+                                            </div>
+                                            <div style="margin-bottom:15px">
+                                                <img alt="Logo" src="assets/media/email/icon-positive-vote-1.svg">
+                                            </div>
+                                            <div style="font-size:14px;font-weight:500;margin-bottom:27px;font-family:Arial,Helvetica,sans-serif">
+                                                <p style="margin-bottom:9px;color:#181C32;font-size:22px;font-weight:700">Hey Marcus, thanks for signing up!</p>
+                                                <p style="margin-bottom:2px;color:#7E8299">Lots of people make mistakes while creating</p>
+                                                <p style="margin-bottom:2px;color:#7E8299">paragraphs. Some writers just put whitespace in</p>
+                                                <p style="margin-bottom:2px;color:#7E8299">their text in random places</p>
+                                            </div>
+                                            <a href='../../demo1/dist/authentication/general/welcome.html' target="_blank" style="background-color:#50cd89;border-radius:6px;display:inline-block;padding:11px 19px;color:#FFFFFF;font-size:14px;font-weight:500;">Activate Account</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr style="display:flex;justify-content:center;margin:0 60px 35px 60px">
+                                    <td align="start" valign="start" style="padding-bottom:10px">
+                                        <p style="color:#181C32;font-size:18px;font-weight:600;margin-bottom:13px">What’s next?</p>
+                                        <div style="background:#F9F9F9;border-radius:12px;padding:35px 30px">
+                                            <div style="display:flex">
+                                                <div style="display:flex;justify-content:center;align-items:center;width:40px;height:40px;margin-right:13px">
+                                                    <img alt="Logo" src="assets/media/email/icon-polygon.svg">
+                                                    <span style="position:absolute;color:#50CD89;font-size:16px;font-weight:600;">1</span>
+                                                </div>
+                                                <div>
+                                                    <div>
+                                                        <a href="#" style="color:#181C32;font-size:14px;font-weight:600;font-family:Arial,Helvetica,sans-serif">Complete your account</a>
+                                                        <p style="color:#5E6278;font-size:13px;font-weight:500;padding-top:3px;margin:0;font-family:Arial,Helvetica,sans-serif">Lots of people make mistakes while creating paragraphs Some writers just put whitespace in their text</p>
+                                                    </div>
+                                                    <div class="separator separator-dashed" style="margin:17px 0 15px 0"></div>
+                                                </div>
+                                            </div>
+                                            <div style="display:flex">
+                                                <div style="display:flex;justify-content:center;align-items:center;width:40px;height:40px;margin-right:13px">
+                                                    <img alt="Logo" src="assets/media/email/icon-polygon.svg">
+                                                    <span style="position:absolute;color:#50CD89;font-size:16px;font-weight:600;">2</span>
+                                                </div>
+                                                <div>
+                                                    <div>
+                                                        <a href="#" style="color:#181C32;font-size:14px;font-weight:600;font-family:Arial,Helvetica,sans-serif">Communication Tool</a>
+                                                        <p style="color:#5E6278;font-size:13px;font-weight:500;padding-top:3px;margin:0;font-family:Arial,Helvetica,sans-serif">Craft a headline that is both informative and will capture readers’ important attention</p>
+                                                    </div>
+                                                    <div class="separator separator-dashed" style="margin:17px 0 15px 0"></div>
+                                                </div>
+                                            </div>
+                                            <div style="display:flex">
+                                                <div style="display:flex;justify-content:center;align-items:center;width:40px;height:40px;margin-right:13px">
+                                                    <img alt="Logo" src="assets/media/email/icon-polygon.svg">
+                                                    <span style="position:absolute;color:#50CD89;font-size:16px;font-weight:600;">3</span>
+                                                </div>
+                                                <div>
+                                                    <div>
+                                                        <a href="#" style="color:#181C32;font-size:14px;font-weight:600;font-family:Arial,Helvetica,sans-serif">Task Planner</a>
+                                                        <p style="color:#5E6278;font-size:13px;font-weight:500;padding-top:3px;margin:0;font-family:Arial,Helvetica,sans-serif">Use images to enhance your post, improve its flow, add humor, and explain complex topics</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" valign="center" style="font-size:13px;text-align:center;padding:0 10px 10px 10px;font-weight:500;color:#A1A5B7;font-family:Arial,Helvetica,sans-serif">
+                                        <p style="color:#181C32;font-size:16px;font-weight:600;margin-bottom:9px">It’s all about customers!</p>
+                                        <p style="margin-bottom:2px">Call our customer care number: +31 6 3344 55 56</p>
+                                        <p style="margin-bottom:4px">You may reach us at
+                                        <a href="https://devs.keenthemes.com" rel="noopener" target="_blank" style="font-weight:600">devs.keenthemes.com</a>.</p>
+                                        <p>We serve Mon-Fri, 9AM-18AM</p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" valign="center" style="text-align:center;padding-bottom:20px">
+                                        <a href="#" style="margin-right:10px">
+                                            <img alt="Logo" src="assets/media/email/icon-linkedin.svg">
+                                        </a>
+                                        <a href="#" style="margin-right:10px">
+                                            <img alt="Logo" src="assets/media/email/icon-dribbble.svg">
+                                        </a>
+                                        <a href="#" style="margin-right:10px">
+                                            <img alt="Logo" src="assets/media/email/icon-facebook.svg">
+                                        </a>
+                                        <a href="#">
+                                            <img alt="Logo" src="assets/media/email/icon-twitter.svg">
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" valign="center" style="font-size:13px;padding:0 15px;text-align:center;font-weight:500;color:#A1A5B7;font-family:Arial,Helvetica,sans-serif">
+                                        <p>&copy; Copyright KeenThemes.
+                                        <a href="https://keenthemes.com" rel="noopener" target="_blank" style="font-weight:600;font-family:Arial,Helvetica,sans-serif">Unsubscribe</a>&nbsp; from newsletter.</p>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
